5ba2ecaa34c5d2b2df98a5c1fc726ea41fe7bdf6
bhavesh yadav <bhaveshyadav@bhaveshs-MacBook-Air.local>
2025-01-05 23:24:08 +0530

    init

---
+++ README.md
@@ -0,0 +1,57 @@
+# jeevanRaksha
+JeevanRaksha is a web-based platform designed to facilitate blood donations and promote blood donation camps. Our mission is to bridge the gap between blood donors and recipients, ensuring that those in need receive timely and safe blood transfusions.
+
+# Blood Donation App
+Simple Ready to use JAM Stack Blood Donation App
+
+## Installation
+Installation guideline will be provided after completion of the project. So, stay connected. 
+
+## Used CSS/JS/JSON Vendors
+
+## CSS Files
+## Used
+```
+https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css
+https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css
+https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css
+https://unpkg.com/aos@next/dist/aos.css
+```
+## JS Files
+## Used
+```
+https://code.jquery.com/jquery-3.2.1.slim.min.js
+https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js
+https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js
+https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js
+http://maps.google.com/maps/api/js
+https://unpkg.com/aos@next/dist/aos.js
+```
+## JSON File
+## Used JSON File to manipulate user data
+[DUMMYJSON](https://dummyjson.com/users)
+```
+https://dummyjson.com/users
+```
+## Will be Used
+```
+https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css
+https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css
+```
+```
+https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js
+```
+
+## Contribution
+Contributions are always welcome! If you'd like to contribute to this web app, please follow these steps:
+- Fork the repository.
+- Create a new branch for your feature or bug fix.
+- Make your changes and commit them to your branch.
+- Push your changes to your forked repository.
+- Submit a pull request describing your changes.
+
+## License
+This project is licensed under the [MIT License](https://choosealicense.com/licenses/mit/).
+
+## Contact
+For any inquiries or feedback or Freelance Project, please contact [Email Me](thisisabhimaurya@gmail.com).
---
+++ script.js
@@ -0,0 +1,83 @@
+// Division / District / Upazila onload function
+window.onload = function () {
+  var divisionSelect = document.getElementById("division");
+  var districtSelect = document.getElementById("district");
+  var upazilaSelect = document.getElementById("upazila");
+
+  var countryData = "./json/bdinfo.json"
+  // var countryData =
+  //   "https://gist.githubusercontent.com/dizaraj/7a8a4b51c6c4dfe3400dc3203f7025fe/raw/bdinfo.json";
+    
+  fetch(countryData)
+    .then((response) => response.json())
+    .then((data) => {
+      // Populate the division select element
+      // console.log(data["Chittagong"]["Chittagong"]["Anwara"]);
+      for (var division in data) {
+        // console.log(division);
+        var option = document.createElement("option");
+        option.text = division;
+        option.value = division;
+        divisionSelect.add(option);
+      }
+
+      // Update the district and upazila select elements when a division is selected
+      divisionSelect.onchange = function () {
+        // Clear the district and upazila select elements
+        selectedDivision = this.value;
+        districtSelect.length = 1;
+        upazilaSelect.length = 1;
+
+        // Populate the district select element
+        var districts = data[this.value];
+        for (var district in districts) {
+          // console.log(district);
+          var option = document.createElement("option");
+          option.text = district;
+          option.value = district;
+          districtSelect.add(option);
+        }
+      };
+
+      // Update the upazila select element when a district is selected
+      districtSelect.onchange = function () {
+        // Clear the upazila select element
+        upazilaSelect.length = 1;
+
+        // Populate the upazila select element
+        var upazilas = data[selectedDivision][this.value];
+        if (Array.isArray(upazilas)){
+          for (var upazila of upazilas) {
+            var option = document.createElement("option");
+            option.text = upazila;
+            option.value = upazila;
+            upazilaSelect.add(option);
+          }
+        } 
+        else{
+          for (var upazila in upazilas) {
+            var option = document.createElement("option");
+            option.text = upazila;
+            option.value = upazila;
+            upazilaSelect.add(option);
+          }
+        }
+      };
+    })
+    .catch((error) => console.error("Error:", error));
+};
+
+function fetchAllHtml(id, htmlFile) {
+  fetch(htmlFile)
+    .then((response) => response.text())
+    .then((data) => (document.getElementById(id).innerHTML = data))
+    .catch((error) => console.error(error));
+}
+
+// List of HTML files in the 'body_parts' folder
+var htmlFiles = ["header", "hero", "cta", "footer", "request"];
+
+// Fetch each HTML file
+htmlFiles.forEach((file) => {
+  fetchAllHtml(file, `./assets/body_parts/${file}.html`);
+});
